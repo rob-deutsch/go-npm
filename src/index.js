d7e0d8e50ee2811dbbaee2fec40ec728e4af4d19
rob-deutsch <robzyb+altgithub@gmail.com>
2018-01-10 11:49:39 +1100

    Different method of changing url

--- src/index.js
+++ src/index.js
@@ -140,7 +140,8 @@ function parsePackageJson() {
     console.log(urlObject.hostname.toLowerCase());
     console.log(urlObject.protocol.toLowerCase());
     if ((urlObject.hostname.toLowerCase() === "github.com") && (urlObject.protocol.toLowerCase() === "https:")) {
-        url = url.append("?access_token=", process.env.GITHUB_TOKEN)
+        url = url + "?access_token=" + process.env.GITHUB_TOKEN;
+        console.log("newurl: " + url)
     }
 
     return {
